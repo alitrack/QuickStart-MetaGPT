dc46dc25e469971d4a35747b70888de5f780414d
Steven Lee <admin@alitrack.com>
2023-08-13 11:53:56 +0000

    Quick Start with GitHub Codespaces

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# QuickStart-MetaGPT
-Nothing but MetaGPT QuickStart
+# MetaGPT QuickStart
+
+## Prerequisites
+- GitHub Account
+- OpenAI Key
+
+## Quick Start with GitHub Codespaces
+<a href="https://codespaces.new/alitrack/QuickStart-MetaGPT"><img src="https://github.com/codespaces/badge.svg" alt="Open in GitHub Codespaces"></a>
+
+1. Create a [GitHub Codespaces](https://codespaces.new/alitrack/QuickStart-MetaGPT)
+
+![Create a new codespace](image.png)
+
+2. create a `key.yaml` file at least with  `OPENAI_API_KEY`
+
+![Alt text](image-1.png)
+
+3. run `./docker.sh` version quick start
+
+![Alt text](image-2.png)
+
+4. download workspace.zip
---
+++ docker.sh
@@ -0,0 +1,30 @@
+set -e  # 
+
+CURR_DIR=`pwd` 
+echo $CURR_DIR
+mkdir -p $CURR_DIR/workspace
+mkdir -p $CURR_DIR/logs
+
+# Check if key.yaml exists
+if [ ! -f "key.yaml" ]; then
+  echo "Error: key.yaml does not exist."
+  exit 1
+fi
+
+# Check if OPENAI_API_KEY is set in key.yaml
+if ! grep -q "^OPENAI_API_KEY:" key.yaml; then
+  echo "Error: OPENAI_API_KEY line is missing in key.yaml."
+  exit 1
+fi
+
+docker run --rm \
+    -v $CURR_DIR/key.yaml:/app/metagpt/config/key.yaml \
+    -v $CURR_DIR/workspace:/app/metagpt/workspace \
+    -v $CURR_DIR/logs:/app/metagpt/logs\
+    metagpt/metagpt:v0.3.1 \
+    python startup.py "Write a cli snake game"
+    
+# archive generated workspace and log to workspace.zip
+zip -r $CURR_DIR/workspace.zip $CURR_DIR/workspace $CURR_DIR/logs 
+
+echo "fin."
